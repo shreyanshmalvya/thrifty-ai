a09638438b3c906e90fef8d9d7ea4d94120b35fd
Shreyansh Malviya <shreyanshmalvya@gmail.com>
2021-12-04 18:20:45 +0530

    Updated animation in Navabar and completed static navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import Navbar from '../comonents/navbar/Navbar'
+import Navbar from '../comonents/navbar/Navbar';
+import Hero from '../comonents/hero/Hero';
 
 const Home = () => {
     return (
         <div>
             <Navbar />
+            <Hero />
         </div>
     )
 }

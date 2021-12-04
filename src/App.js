3706b8286ee6cab8fb025d4a3b450f6a0656c217
Shreyansh Malviya <shreyanshmalvya@gmail.com>
2021-12-04 15:42:03 +0530

    Updated Navbar component and added Scss file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Home />
     </div>
   );
 }
--- src/comonents/navbar/Navbar.jsx
+++ src/comonents/navbar/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
-
+import './navbar.css'
+const PF = process.env.REACT_APP_PUBLIC_FOLDER;
 
 const Navbar = () => {
     return (
-        <div>
-            <div className="navbar">
-                <ul>
-                    <li><img src="./assets/img/Tlogo.png" width="120" height="55" class="d-inline-block align-top align-middle"
-                        alt="Thrifty logo" /></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
+        <div className="navbar">
+            <div className='navabarLogo'>
+                <img src={PF + 'logo.png'} alt="Thrifty logo" className='expandableLogo' />
+                <div>Thrifty AI</div>
             </div>
+            <ul className='navbarElements'>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>Technology</a></li>
+                <li><a href='#'>Contact Us</a></li>
+                <li><a href='#'>Blogs</a></li>
+            </ul>
         </div>
     )
 }

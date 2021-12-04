b6d990a637812960d0263d24d162ead7f242acc0
Shreyansh Malviya <shreyanshmalvya@gmail.com>
2021-12-04 14:18:32 +0530

    Feat : Added env file

---
+++ src/comonents/navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="navbar">
+                <ul>
+                    <li><img src="./assets/img/Tlogo.png" width="120" height="55" class="d-inline-block align-top align-middle"
+                        alt="Thrifty logo" /></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar

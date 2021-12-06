30c1180eaada206e2086f383919afafd9b829cb2
Dikshansh Kumar Lodhi <dikshansh1712@gmail.com>
2021-12-06 20:01:49 +0530

    Hero: Added HTML

--- src/comonents/hero/Hero.jsx
+++ src/comonents/hero/Hero.jsx
@@ -1,26 +1,39 @@
-import React from 'react'
 import './hero.css'
+import tryImg1 from "../../images/tryImg1.png"
+import tryImg2 from "../../images/tryImg2.png"
+import tryImg3 from "../../images/tryImg3.png"
 
-const Hero = () => {
+
+export default function Hero() {
     return (
-        <div className="heroWrapper">
-            <div className='introHeader'>
-                <div className='introHeading'>
-                    <div>EVER MET AN</div>
-                    <div className='glitch'>
-                        <span aria-hidden="true">ARTIFICAL HUMAN </span>
-                        ARTIFICAL HUMAN
-                        <span aria-hidden="true">ARTIFICAL HUMAN </span>
+        <div>
+            <div className="heroWrapper">
+                <div className="heroLeft">
+                    <img src={tryImg1} alt="" />
+                    <img src={tryImg2} alt="" />
+                    <img src={tryImg3} alt="" />
+                </div>
+                <div className="heroRight">
+                    <div className="rTop">
+                        <div className="glitch">
+                            <span aria-hidden="true">Ever met</span>
+                            Ever met
+                            <span aria-hidden="true">Ever met</span>
+                        </div>
+                        <div className="glitch">
+                            <span aria-hidden="true">artifical human</span>
+                            artifical human
+                            <span aria-hidden="true">artifical human</span>
+                        </div>
                     </div>
+
+                    <div className="rBottom">
+                        <p>AI Humans are AI powered characters that can
+                        see, hear and understand. They look like us, talk like us and behave like us</p>
+                    </div>
+
                 </div>
-                <div className='heroInfo'>
-                    AI Humans are AI powered characters that can
-                    see, hear and understand. They look like us, talk like us and behave like us
-                </div>
-                <button className='learnMore'>Learn More</button>
             </div>
         </div>
     )
 }
-
-export default Hero

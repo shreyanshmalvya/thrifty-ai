7ca8040e2690f170bc02d6f90f453dfb3c610248
Shreyansh Malviya <shreyanshmalvya@gmail.com>
2021-12-07 14:31:20 +0530

    Updated responsive navbar for 576px

--- src/comonents/navbar/Navbar.jsx
+++ src/comonents/navbar/Navbar.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 const PF = process.env.REACT_APP_PUBLIC_FOLDER;
 
 const Navbar = () => {
+
+    const [toggle,setToggle] = useState(false);
+    const [sideBar,setSideBar] = useState(false)
+    console.log(toggle);
+
     return (
         <div className="navbar">
             <div className='navabarLogo'>
                 <img src={PF + 'logo.png'} alt="Thrifty logo" className='expandableLogo' />
                 <div>THRIFTY AI</div>
             </div>
-            <ul className='navbarElements'>
-                <li className="nav-item">HOME</li>
-                <li className="nav-item">TECHNOLOGY</li>
-                <li className="nav-item">CONTACT US</li>
-                <li className="nav-item">BLOGS</li>
+            <ul className={`navbarElements ${sideBar? '' : 'sideBarHide'}`}>
+                <li className={`nav-item ${sideBar? '' : 'nav-item-slide'}`}>HOME</li>
+                <li className={`nav-item ${sideBar? '' : 'nav-item-slide'}`}>TECHNOLOGY</li>
+                <li className={`nav-item ${sideBar? '' : 'nav-item-slide'}`}>CONTACT US</li>
+                <li className={`nav-item ${sideBar? '' : 'nav-item-slide'}`}>BLOGS</li>
             </ul>
+            
+            {toggle? null :
+    
+            <div className='sideDrawer'>
+                <button className='toggleButton' onClick = {() => {setToggle(false); setSideBar(!sideBar)}}>
+                    <div className="toggleButtonLine"></div>
+                    <div className="toggleButtonLine"></div>
+                    <div className="toggleButtonLine"></div>
+                </button>
+            </div>
+            }
         </div>
     )
 }

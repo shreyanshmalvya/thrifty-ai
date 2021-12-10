7a48e70cda5c34a1c290251cc41492d73f009fd0
Shreyansh Malviya <shreyanshmalvya@gmail.com>
2021-12-10 12:26:42 +0530

    Added AI render videos and updated intro component

--- src/components/introAI/IntroAI.jsx
+++ src/components/introAI/IntroAI.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 const IntroAI = () => {
     return (
-        <div>
-            
+        <div className="grid">
+            <div className='box'>
+                <div className="data1"></div>
+            </div>
+            <div className='box'>
+                <div className="data2">
+                    <span>WHY AI HUMANS ?</span>
+                    <span>​Digital Humans are a huge leap in brand and customer experience, combining the best of online and face-to-face interaction. They are the next generation human form of chatbots</span>
+                </div>
+            </div>
+            <div className='box'>
+                <div className="data3">
+                    <span>REDEFINE YOUR CX</span>
+                    <span>No more clicks on forms, wait to get calls connected to the customer service, take your business face to face.​“Namaste” in the language your customer speaks. Every customer is different, so are their needs. Personalize and present with the best customer experience anywhere/anytime.</span>
+                </div>
+            </div>
+            <div className='box'>
+                <div className="data4"></div>
+            </div>
         </div>
     )
 }

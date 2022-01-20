82557499d15e654b8e437c8125ef092c6212aa69
Dikshansh Kumar Lodhi <dikshansh1712@gmail.com>
2022-01-20 21:15:31 +0530

    aos mobile disabled

--- src/components/introAI/IntroAI.jsx
+++ src/components/introAI/IntroAI.jsx
@@ -8,7 +8,7 @@ import "aos/dist/aos.css";
 
 const IntroAI = () => {
     useEffect(() => {
-        AOS.init();
+        AOS.init({disable: 'mobile'});
         AOS.refresh();
     }, []);
     return (

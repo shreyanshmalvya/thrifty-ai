efed0f7a65b68c47816a33b9d1ea2aa65b6cf062
Shreyansh Malviya <shreyanshmalvya@gmail.com>
2022-01-20 18:12:55 +0530

    Added images

--- src/components/introAI/IntroAI.jsx
+++ src/components/introAI/IntroAI.jsx
@@ -5,29 +5,29 @@ import cap2 from "../../assets/cap2.png"
 
 const IntroAI = () => {
     return (
-        <>
-        <div className="introAIHeader">INTRODUCING ARTIFICIAL HUMANS</div>
-        <div className="grid">
-            <div className="introAITop">
-                <div className='introAITopLeft'>
-                    <img src={cap1} alt='GlazeIntoFuture' />
-                </div>
-                <div className='introAITopRight'>
+        <div className="introWrapper">
+            <div className="introAIHeader">INTRODUCING ARTIFICIAL HUMANS</div>
+            <div className="grid">
+                <div className="introAITop">
+                    <div className='introAITopLeft'>
+                        <img src={cap1} alt='GlazeIntoFuture' />
+                    </div>
+                    <div className='introAITopRight'>
                         <span>WHY AI HUMANS ?</span>
                         <span>​Digital Humans are a huge leap in brand and customer experience, combining the best of online and face-to-face interaction. They are the next generation human form of chatbots</span>
+                    </div>
+                </div>
+                <div className="introAIBottom">
+                    <div className='introAIBottomLeft'>
+                        <span>REDEFINE YOUR CX</span>
+                        <span>No more clicks on forms, wait to get calls connected to the customer service, take your business face to face.​“Namaste” in the language your customer speaks. Every customer is different, so are their needs. Personalize and present with the best customer experience anywhere/anytime.</span>
+                    </div>
+                    <div className='introAIBottomRight'>
+                        <img src={cap2} alt='nightCity' />
+                    </div>
                 </div>
             </div>
-            <div className="introAIBottom">
-            <div className='introAIBottomLeft'>
-                    <span>REDEFINE YOUR CX</span>
-                    <span>No more clicks on forms, wait to get calls connected to the customer service, take your business face to face.​“Namaste” in the language your customer speaks. Every customer is different, so are their needs. Personalize and present with the best customer experience anywhere/anytime.</span>
-            </div>
-            <div className='introAIBottomRight'>
-                <img src={cap2} alt='nightCity' />
-            </div>
-            </div>
         </div>
-        </>
     )
 }
 

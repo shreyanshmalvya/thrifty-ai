d6278efa2b515a78022ed8f0811750712ddc242e
Dikshansh Kumar Lodhi <dikshansh1712@gmail.com>
2022-01-20 19:19:27 +0530

    aos added in HeroPage

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,14 +1,21 @@
 import './hero.css'
 import WelcomeVid from "../../assets/Videos/WelcomeVid.mp4"
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 
 export default function Hero() {
+    useEffect(() => {
+        AOS.init();
+        AOS.refresh();
+    }, []);
     return (
         <div className="heroWrapper">
-            <div className="heroLeft">
+            <div data-aos="fade-right" data-aos-duration="500" className="heroLeft">
                 <video loop autoPlay muted src={WelcomeVid}></video>
             </div>
-            <div className="heroRight">
+            <div data-aos="fade-left" data-aos-duration="500" data-aos-delay="600" className="heroRight">
                 <div className="heroRTop">
                     <p className="heroGlitchHeading">Ever met an</p>
                     <div className="glitch">
@@ -20,7 +27,7 @@ export default function Hero() {
                     <p>AI Humans are AI powered characters that can
                     see, hear and understand. They look like us, talk like us and behave like us</p>
 
-                    <span className="heroButton"><a href="#">Learn More</a></span>
+                    <span className="heroButton"><a href="/#">Learn More</a></span>
                 </div>
 
             </div>
